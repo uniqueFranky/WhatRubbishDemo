6ded0499e5aae709b9d445ea80e8d63180792a94
uniqueFranky <y_r_b@qq.com>
2022-04-07 18:21:47 +0800

    Initial Commit

---
+++ WhatRubbishDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatRubbishDemo
+//
+//  Created by 闫润邦 on 2022/4/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
